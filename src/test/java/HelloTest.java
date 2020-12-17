168c377adede5faae041c1c4a90dba2a3b073c58
makist73 <68976065+makist73@users.noreply.github.com>
2020-12-16 21:32:34 -0500

    new test

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,20 +1,44 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class HelloTest {
 
+    // Junit5 Annotations
+    // @BeforeAll @AfterAll @BeforeEach @AfterEach
+    // @DisplayName @Disabled
+
+   @BeforeAll
+   public static void  setup(){
+       System.out.println("@BeforeAll is running");
+   }
+
+   @AfterAll
+   public static void tearDown(){
+       System.out.println("@AfterAll is running");
+   }
+
+   @BeforeEach
+   public void setUpTest(){
+       System.out.println("@BeforeEach is running");
+   }
+
+   @AfterEach
+   public void tearDownTest(){
+       System.out.println("@AfterEach is running");
+   }
 
     @Test
     public void test(){
+        System.out.println("test 1 is running");
         Assertions.assertEquals(4,1+3);
 
     }
 
     @Test
     public void test2(){
+        System.out.println("test 2 is running");
         // assert 4 times 3 is 12
         assertEquals(12, 4*3);
     }
