61913e83af8cc14edb416686b615d6159135a347
makist73 <68976065+makist73@users.noreply.github.com>
2020-12-19 14:05:40 -0500

    day04 Rest_Assured

--- src/test/java/day04/OpenMovieDB_Test.java
+++ src/test/java/day04/OpenMovieDB_Test.java
@@ -1,12 +1,9 @@
 package day04;
 
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+
 import org.junit.jupiter.api.*;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 
-import java.util.List;
 
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
@@ -18,7 +15,7 @@ public class OpenMovieDB_Test {
 
     @BeforeAll
     public static void setUp(){
-        baseURI="http://www.omdbapi.com?t=The Orville";
+        baseURI="http://www.omdbapi.com";
     }
     @AfterAll
     public static void tearDown(){
@@ -29,5 +26,43 @@ public class OpenMovieDB_Test {
     @Test
     public void testMovie(){
 
+        given()
+                .queryParam("apikey", "89e2e4e7")
+                .queryParam("t", "The Orville").
+
+        when()
+                .get().prettyPeek().   // our request URL is already complete , do not need to add anything here
+
+        then()
+                .statusCode(is(200))
+                .contentType(ContentType.JSON)
+                .body("Title", is("The Orville"))
+                .body("Ratings[0].Source" ,is("Internet Movie Database"));
+
+    }
+
+    @DisplayName("Getting the log of request and response")
+    @Test
+    public void testSendingRequestAndGetTheLog(){
+        given()
+                .queryParam("apikey", "89e2e4e7")
+                .queryParam("t", "John Wick")
+                // logging the request should be in given section
+                .log().all().
+//                .log().uri().
+//                  .log().params().
+        when()
+                .get().
+        then()
+                // logging the response should be in then section
+//                .log().all()
+//                .log().status()
+//                .log().body()
+                .log().all()
+                .statusCode(is(200))
+                .body("Plot",containsString("ex-hit-man"))
+                // second Ratings source is Rotten Tomato
+                .body("Ratings[1].Source",is("Rotten Tomatoes") )
+                ;
     }
 }
