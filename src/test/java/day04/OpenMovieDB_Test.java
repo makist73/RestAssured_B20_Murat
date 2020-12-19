34f29f7acecd1324ca23b1453900e24b13dbdd02
makist73 <68976065+makist73@users.noreply.github.com>
2020-12-19 11:33:17 -0500

    day04 Rest_Assured

--- src/test/java/day04/OpenMovieDB_Test.java
+++ src/test/java/day04/OpenMovieDB_Test.java
@@ -1,5 +1,6 @@
 package day04;
 
+import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.*;
 import io.restassured.http.ContentType;
@@ -13,11 +14,20 @@ import static org.hamcrest.Matchers.* ;
 
 public class OpenMovieDB_Test {
 
-    // http://www.omdbapi.com?t=The Orville
+        // http://www.omdbapi.com?t=The Orville
 
     @BeforeAll
     public static void setUp(){
+        baseURI="http://www.omdbapi.com?t=The Orville";
+    }
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
 
+    @DisplayName("Test Search Movie or OpenMovieDB Test")
+    @Test
+    public void testMovie(){
 
     }
 }
